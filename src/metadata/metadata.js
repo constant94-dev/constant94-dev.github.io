bb32a5dba287cf078aa426557d1bfdd41c6a2ac5
sjpark0205 <sangjun.park@bodycodi.com>
2024-04-24 16:20:50 +0900

    [240424] 이력 수정 및 전체 폼 변경

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -7,48 +7,67 @@ module.exports = {
     'Tistory': '<a href="https://doit94.tistory.com/"><i class="fa fa-brands fa-blogger fact-icon"></i>Just Do it!</a>',
     'Email': '<a href="mailto:brainstorm2973@gmail.com"><i class="fa fa-envelope fact-icon"></i>brainstorm2973@gmail.com</a>'
   },
-  positions: [
+  redblue_positions: [
+    {
+      title: '외부장치 연동관리 개발.',
+      period: '(1 month)',
+      skills: ['Java','Spring MVC','Mybatis','MariaDB','AWS'],
+      problem: `<Situation>`,
+      problem_contents: `장치 연동 데이터를 DBA가 직접 DB 작업 수행`,
+      cause: `<Solution>`,
+      cause_contents: [
+        'Spring Boot api 를 이용하여 CRUD 전반적인 라이프사이클에 대해서 설계하고 지속적인 리팩토링을 통해 API를 RESTful 하게 구성'
+      ],
+      effect: `<Impact>`,
+      effect_contents: `ADMIN 페이지에서 운영팀이 직접 데이터를 관리할 수 있게 제공해 운영 효율 개선`
+    }
+  ],
+  nodamen_skills: [
+    {
+      skills: ['Java','Spring MVC','MySQL','Terraform','Azure']
+    }
+  ],
+  nodamen_positions: [
     {
       title: '빌드 & 배포 자동화 구축.',
-      period: '2023.04 - 2023.07 (4개월)',
+      period: '(4 month)',
       skills: ['Terraform', 'Azure', 'ACR', 'AKS'],
       problem: `<Situation>`,
       problem_contents: `빌드 & 배포로 소요되는 시간이 평균 90분 발생`,
       cause: `<Solution>`,
       cause_contents: [
-        '소스코드를 빌드하고 해당 버전을 이미지화하고 호스트 서버의 배포하는 프로세스의 자동화 필요성을 확인',
         'Terraform 이용해 코드기반 Azure Resource 배포 구성하여 동일한 환경 프로비저닝하도록 보장',
         'Azure Pipelines 를 이용해 파이프라인 구축',
         '도커라이징해서 ACR(Azure Container Registry)로 형상관리 및 AKS(Azure Kubernetes Service) 배포'
       ],
       effect: `<Impact>`,
-      effect_contents: `DevOps 구성을 통한 기존 소요시간 대비 90분 👉 5분 개선, QA에서 찾아낸 버그를 빠르게 해결할수 있게 되어 일정 영향없이 필요한 시간내에 제품을 전달`
+      effect_contents: `기존 소요시간 대비 90분 👉 5분 개선, QA에서 찾아낸 버그를 빠르게 반영해 일정 영향없이 필요한 시간내에 제품 전달`
     },
     {
       title: '파트론 웹 사이트 개발.',
-      period: '2021.11 - 2022.08 (10개월)',
+      period: '(10 month)',
       skills: ['Java', 'Spring Boot', 'JPA', 'MySQL'],
       problem: `<Situation>`,
-      problem_contents: `개발팀 재구성과 예술 감상 서비스 레거시 청산`,
+      problem_contents: `예술 감상 서비스 레거시 청산`,
       cause: `<Solution>`,
       cause_contents: [
-        '요구사항 기획서를 기반으로 개발 타임라인 수립',
-        'JPA 메서드 쿼리 전략으로 서비스 쿼리를 쿼리 메서드 형태로 모두 변경',
-        'Spring Boot 에서 제공하는 어노테이션을 사용하여 불필요한 로직과 요청을 제거',
-        'Spring Boot api 를 이용하여 생성, 조회, 수정, 삭제 도메인의 전반적인 라이프사이클에 대해서 설계하고 지속적인 리팩토링을 통해 API를 RESTful 하게 구성'
+        '파트론 서비스에 사용되는 API 서버 개발',
+        '기존 PHP로 작성된 API를 Java와 Spring Boot로 이전 및 코드 개선',
+        'ORM 도입',
+        'Azure VMSS, AGW, nginx 서버 인프라 사용',
+        'Azure Database for MariaDB를 데이터베이스로 사용'
       ],
       effect: `<Impact>`,
-      effect_contents: `PHP 👉 JAVA 언어 전환, 레거시 청산 완료 후 서비스 체류 시간 10초 👉 1분 증가`
+      effect_contents: `PHP 👉 JAVA 언어 전환, 레거시 청산 완료`
     },
     {
-      title: '레거시 프로젝트 리팩토링.',
-      period: '2021.01 - 2021.03 (3개월)',
+      title: '레거시 쿼리 리팩토링.',
+      period: '(3 month)',
       skills: ['MySQL', 'Sub Query', 'MySQL Workbench'],
       problem: `<Situation>`,
-      problem_contents: `외주업체가 개발한 레거시 프로젝트에서 서비스 운영 중 트래픽이 몰려 성능 저하 발생`,
+      problem_contents: `외주업체가 개발한 레거시 서비스 운영 중 트래픽이 몰려 성능 저하 발생`,
       cause: `<Solution>`,
       cause_contents: [
-      '외주 업체와의 주기적 미팅을 통해 레거시 프로젝트 분석해 과부하 포인트를 리스트업하고 튜닝 계획 수립',
       '필요한 데이터를 다수의 메인쿼리를 사용해 가져온 후 가져온 데이터를 새로운 쿼리의 정의하고 사용되는 패턴 발견',
       '로드의 30초가 넘어가는 페이지를 리스트업하고 해당 페이지의 SQL 튜닝 계획 수립',
       'EXPLAIN 결과 분석하여 Optimizer 쿼리 실행 계획을 확인',
@@ -61,42 +80,41 @@ module.exports = {
   skills: [
     {
       title: 'Backend',
-      skills: ['Java','Spring framework','Spring MVC','JPA','MySQL']
+      skills: ['Java','Spring MVC','JPA','Mybatis','MySQL']
     },
     {
       title: 'DevOps',
-      skills: ['GitHub','Azure','Akamai CDN']
+      skills: ['GitHub','Azure','AWS','Akamai CDN','Jenkins']
     },
     {
       title: 'Collaboration',
-      skills: ['Google Workspace','Figma']
+      skills: ['Confluence','Jira','Slack','Google Workspace','Figma']
     }
   ],
   experience: [
     {
       header: '우테코 프리코스',
       skills: ['코드 컨벤션', '테스트 코드', '클래스 분리'],
-      contents: ` 2023.10 - 2023.11 | Junit5 테스트 코드 작성과 가독성 높은 객체지향 코드 작성을 위한 경험 `
+      period: '2023.10 - 2023.11',
+      contents: `Junit5 테스트 코드 작성과 가독성 높은 코드 작성 `
     },
     {
       header: 'AWS Bootcamp EKS',
       skills: ['EKS', 'Observability'],
-      contents: ` 2023.08 | 사용자 트래픽이 증가할 때 인프라 구성에 궁금증을 가지고 Bootcamp 경험 `
-    },
-    {
-      header: '코딩 독학 및 팀 프로젝트',
-      skills: ['아두이노', '라즈베리파이', '안드로이드'],
-      contents: ` 2019.11 - 2020.11 | 다양한 개발 경험을 하고싶어 공부하면서 알게된 사람들과 팀 프로젝트를 경험 `
+      period: '2023.08',
+      contents: `사용자 트래픽이 증가할 때 인프라 구성에 궁금증을 가지고 Bootcamp 참여 `
     },
     {
       header: '세종사이버대학교',
       skills: ['CS', 'AWS'],
-      contents: ` 2022.03 - 2023.08 (졸업) | CS(Computer Science) 전공 지식과 AWS 인프라 구성을 실습`
+      period: '2022.03 - 2023.08',
+      contents: `CS(Computer Science) 전공 지식과 AWS 인프라 구성 실습`
     },
     {
       header: '부천대학교',
       skills: ['Network', '아두이노'],
-      contents: ` 2013.03 - 2017.08 (졸업) | Network 관련 전공 지식과 전자회로 실습`
+      period: '2013.03 - 2017.08',
+      contents: `Network 관련 전공 지식과 전자회로 실습`
     }
   ],
   pdf_name: 'aaron_resume',
