3739d8ceec871a1b8ffa9a6ef427b255652e57c3
sjpark0205 <sangjun.park@bodycodi.com>
2024-04-24 18:15:23 +0900

    [240424] 이력 수정 및 전체 폼 2차 변경

--- src/utils/pdf.js
+++ src/utils/pdf.js
@@ -3,17 +3,18 @@ const Puppeteer = require('puppeteer');
 module.exports = async function buildPdf(inputFile, outputFile) {
   const browser = await Puppeteer.launch();
   const page = await browser.newPage();
+
   await page.goto(`file://${inputFile}`, {
-    waitUntil: 'networkidle0'
+    waitUntil: 'networkidle2'
   });
   await page.pdf({
     path: outputFile,
-    format: 'A4',
+    format: 'A3',
     border: 0,
     margin: {
-      top: '2.00cm',
+      top: '2.54cm',
       right: '2.54cm',
-      bottom: '1.50cm',
+      bottom: '2.54cm',
       left: '2.54cm',
     },
   });
